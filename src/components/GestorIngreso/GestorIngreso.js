ef45ce775a888a8addb4c8c75cf2983fa27e2d27
Fabrizio99 <avbar357159@gmail.com>
2020-09-18 01:50:41 -0500

    styles on entermanagment and login - done

--- src/components/GestorIngreso/GestorIngreso.js
+++ src/components/GestorIngreso/GestorIngreso.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 
+import { makeStyles} from '@material-ui/core/styles';
 import Login from './../Login/Login';
 import Register from './../Register/Register';
+const useStyles = makeStyles((theme) => ({
 
-function GestorIngreso() {
+  buttonContainer : {
+    marginTop: '60px',
+    display: 'flex',
+    flexDirection: 'column',
+    '& > Button' : {
+      borderRadius : '5px',
+      padding : '10px 15px',
+      width: '300px',
+      fontWeight : '500',
+      boxShadow : 'rgba(0,0,0,0.2) 1px 1px 5px 0',
+    },
+    '& > Button:first-child':{
+      marginBottom : '10px'
+    }
+  },
+  blueButton : {
+    backgroundColor : '#2E598C',
+    color : 'white',
+    '&:hover' : {
+      backgroundColor : '#2E598C',
+    }
+  },
+  whiteButton : {
+    backgroundColor : 'white',
+    color : '#686868',
+    '&:hover' : {
+      backgroundColor : '#EBE9E9',
+    }
+  },
+}));
+
+function GestorIngreso(props) {
+  const classes = useStyles();
   const [openLogin, setOpenLogin] = React.useState(false);
   const [openRegister, setOpenRegister] = React.useState(false);
 
@@ -34,16 +68,15 @@ function GestorIngreso() {
   return (
     <div class="main-container">
       <div class="part1" style={{ backgroundImage:`url(${process.env.PUBLIC_URL + '/images/portada_login2.jpg'})` }}>
-        {/* <img src={process.env.PUBLIC_URL + '/images/portada_login2.jpg'} alt="imagen portada" className="imagen-ingreso" /> */}
       </div>
       <div class="part2">
         <div class="part2-content">
-          <h1>Crea y construye proyectos con gente como tu</h1>
-          <div class="btn-content">
-            <Button variant="contained" color="primary" onClick={handleLogin}>
-              Iniciar Sesion
+          <h1>Crea y construye proyectos con gente como tú</h1>
+          <div class="btn-main-content" className={classes.buttonContainer}>
+            <Button variant="contained" color="primary" onClick={handleLogin} className={classes.blueButton}>
+              Iniciar Sesión
             </Button>
-            <Button variant="contained" color="secondary" onClick={handleRegister}>
+            <Button variant="contained" color="secondary" onClick={handleRegister} className={classes.whiteButton}>
               Registrarse
             </Button>
           </div>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,17 +2,59 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
 import Button from '@material-ui/core/Button';
 
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles} from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import { Directions } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
       margin: theme.spacing(1),
-      width: '25ch',
+      width: '300px',
     },
+    display : 'flex',
+    flexDirection : 'column',
+    alignItems : 'center'
+  },
+  'login-title' : {
+    color : '#2E598C',
+    textAlign: 'center',
+    fontWeight : 'bold'
+  },
+  'login-content' : {
+    minWidth: "400px"
+  },
+  loginButtonContainer : {
+    display : 'flex',
+    flexDirection : 'column',
+    alignItems : 'center',
+    '& > *' : {
+      marginBottom : '10px',
+    },
+    '& > Button' : {
+      borderRadius : '5px',
+      padding : '10px 15px',
+      width: '300px',
+      fontWeight : '500',
+      boxShadow : 'rgba(0,0,0,0.2) 1px 1px 5px 0',
+    },
+  },
+  blueButton : {
+    backgroundColor : '#2E598C',
+    color : 'white',
+    '&:hover' : {
+      backgroundColor : '#2E598C',
+    }
+  },
+  whiteButton : {
+    backgroundColor : 'white',
+    color : '#686868',
+    '&:hover' : {
+      backgroundColor : '#EBE9E9',
+    }
   },
 }));
 
@@ -35,37 +77,36 @@ function Login(props) {
   }
 
   return (
-    <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open}>
+    <React.Fragment>
+      <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open} maxWidth="xl" classes={{ paper: classes["login-content"]}}>
+        <DialogTitle id="simple-dialog-title" className={classes["login-title"]}>
+          Iniciar Sesión
+        </DialogTitle>
+        <DialogContent>
+          <form className={classes.root} noValidate autoComplete="off">
 
-      <DialogTitle id="simple-dialog-title">
-        Iniciar Sesion
-      </DialogTitle>
+            <TextField id="filled-basic" label="Correo electronico" variant="filled" />
+            <TextField id="filled-basic" label="Contraseña" variant="filled" />
+          </form>
 
-      <form className={classes.root} noValidate autoComplete="off">
+          <div className={classes.loginButtonContainer}>
+            <Button variant="contained" color="secondary" onClick={handleLogin} className={classes.blueButton}>
+            Iniciar Sesión
+            </Button>
 
-        <TextField id="filled-basic" label="Correo electronico" variant="filled" />
-        <TextField id="filled-basic" label="Contraseña" variant="filled" />
-      </form>
+            <span>ó</span>
 
-        <Button variant="contained" color="secondary" onClick={handleLogin}>
-          Iniciar Sesion
-        </Button>
+            <Button variant="contained" color="secondary" onClick={handle} className={classes.whiteButton}>
+            Iniciar Sesión con Google
+            </Button>
 
-        <span>o</span>
-        
-        <Button variant="contained" color="secondary" onClick={handle}>
-          Google
-        </Button>
-
-        <Button variant="contained" color="secondary" onClick={handle}>
-          Facebook
-        </Button>
-
-        <Button variant="contained" color="secondary" onClick={handle}>
-          Apple
-        </Button>
-
-    </Dialog>
+            <Button variant="contained" color="secondary" onClick={handle} className={classes.whiteButton}>
+            Iniciar Sesión con Facebook
+            </Button>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </React.Fragment>
   );
 }
 
