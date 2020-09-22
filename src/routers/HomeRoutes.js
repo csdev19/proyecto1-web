3a4ade39de904efcc4539cb52310b0ee38da8f55
pystudent1913 <cristiansotomayor1912@gmail.com>
2020-09-21 23:57:13 -0500

    cambie el router

--- src/routers/HomeRoutes.js
+++ src/routers/HomeRoutes.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Navbar } from '../components/ui/Navbar';
 import { Home } from '../components/Home/Home';
 import { Redirect, Route, Switch } from 'react-router-dom';
+import { Profile } from '../components/Profile/Profile';
 
 export const HomeRoutes = () => {
   return (
@@ -12,6 +13,7 @@ export const HomeRoutes = () => {
       <div>
         <Switch>
           <Route exact path="/home" component={ Home } />
+          <Route exact path="/profile" component={ Profile } />
           <Redirect to="/auth" />
         </Switch>
       </div>
